11e69a31fd532c04bb83320fcedab6fa1a927a6c
Alessandro Lenzi <alessandro.lenzi@cynny.com>
2017-03-03 16:51:50 +0100

    fixed test configuration: all tests passing; added cmake-build-debug to gitignore

--- test/configuration_parser_test.cpp
+++ test/configuration_parser_test.cpp
@@ -1,15 +1,19 @@
 #include "../src/configuration/configuration_parser.h"
 #include <gtest/gtest.h>
 
-TEST(configuration_parser, asd)
+TEST(configuration_parser, loading_and_parsing)
 {
 	try
 	{
 		configuration_parser my_parser("./etc/doormat/doormat.test.config");
 		auto cw = my_parser.parse();
 	}
-	catch(...)
+	catch (const std::invalid_argument& invarg)
 	{
-		FAIL();
+		FAIL() << "invalid argument:" << invarg.what();
+	} catch (const std::logic_error &logicerror) {
+        FAIL() << "logic error: " << logicerror.what();   
+    } catch (...) {
+		FAIL() << "an unknown error occurred!";
 	}
 }
