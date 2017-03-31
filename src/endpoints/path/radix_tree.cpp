5e5ae3c2ae16660231cd1d823c21b834f1bdc139
Paolo Monteverde <paolo.monteverde@cynny.com>
2017-03-31 10:14:57 +0200

    improvede readability of radix_tree with some type alias

--- src/endpoints/path/radix_tree.cpp
+++ src/endpoints/path/radix_tree.cpp
@@ -31,8 +31,7 @@ void radix_tree::addPattern(const std::string &path_pattern, generating_function
 	addChild(tokens.begin(), tokens.end(), gen);
 }
 
-void radix_tree::addChild(std::vector<std::string>::iterator begin, 
-	std::vector<std::string>::iterator end, generating_function_t gen) 
+void radix_tree::addChild(vec_it begin, vec_it end, generating_function_t gen)
 {
 	/** - We already match with the previous by inductive hypothesis. So we just have to move forward.*/
 	if(begin == end)
@@ -53,7 +52,7 @@ void radix_tree::addChild(std::vector<std::string>::iterator begin,
 }
 
 //we're sure that begin != end as appendChild is called by addPattern, which checks it in advance.
-void radix_tree::appendChild(std::vector<std::string>::iterator begin, std::vector<std::string>::iterator end, generating_function_t gen) 
+void radix_tree::appendChild(vec_it begin, vec_it end, generating_function_t gen)
 {
 	childs.push_back(std::make_unique<radix_tree>(*begin, splitToken));
 	++begin;
@@ -69,8 +68,8 @@ bool radix_tree::matches(const std::string &path, http::http_request*r) const
 	return bool(matches(path.cbegin(), path.cend(), r));
 }
 
-std::experimental::optional<const radix_tree*> radix_tree::matches(std::string::const_iterator path_it, 
-	std::string::const_iterator end, http::http_request*r) const
+radix_tree::optional_tree radix_tree::matches(str_it path_it,
+	str_it end, http::http_request*r) const
 {
 	while(path_it !=  end && *path_it== splitToken) ++path_it;
 	if(node_label == "*")
@@ -91,7 +90,7 @@ std::experimental::optional<const radix_tree*> radix_tree::matches(std::string::
 	{
 		//it matches, but only with a partial path. so it is as if it does not match :D
 		if(path_iterator == end)
-			return bool(generating_function) ? this : std::experimental::optional<const radix_tree*>{};
+			return bool(generating_function) ? this : radix_tree::optional_tree{};
 		/* Path has not been consumed: demand it to the childs. */
 		
 		for(auto &&c : childs) 
@@ -101,14 +100,14 @@ std::experimental::optional<const radix_tree*> radix_tree::matches(std::string::
 		}
 	}
 
-	return std::experimental::optional<const radix_tree*>{};
+	return radix_tree::optional_tree{};
 }
 
-std::experimental::optional<const radix_tree*> radix_tree::wildcard_matches(std::string::const_iterator path_it, std::string::const_iterator end, http::http_request*r) const
+radix_tree::optional_tree radix_tree::wildcard_matches(str_it path_it, str_it end, http::http_request*r) const
 {
 	auto nextPathBegin = std::find(path_it, end, splitToken);
 	if ( nextPathBegin == end ) 
-		return bool(generating_function) ? this : std::experimental::optional<const radix_tree*>{};
+		return bool(generating_function) ? this : radix_tree::optional_tree{};
 
 	for(auto &&c: childs) 
 	{
@@ -116,18 +115,18 @@ std::experimental::optional<const radix_tree*> radix_tree::wildcard_matches(std:
 		if(bool(tptr)) return tptr;
 	}
 	//if none of the child matches, we match only if * is an available termination.
-	return bool(generating_function) ? this : std::experimental::optional<const radix_tree*>{};
+	return bool(generating_function) ? this : radix_tree::optional_tree{};
 }
 
 
-std::experimental::optional<const radix_tree*> radix_tree::parameter_matches(std::string::const_iterator path_it, std::string::const_iterator end, http::http_request*r) const
+radix_tree::optional_tree radix_tree::parameter_matches(str_it path_it, str_it end, http::http_request*r) const
 {
 	auto nextPathBegin = std::find(path_it, end, splitToken);
 	if(nextPathBegin == end) {
 		if(bool(generating_function)) {
             if(r) r->addParameter(std::string{this->node_label.begin()+1, this->node_label.end()-1}, std::string(path_it, end));
             return this;
-        } return std::experimental::optional<const radix_tree*>{}; //we matched with a parameter.
+		} return radix_tree::optional_tree{}; //we matched with a parameter.
     }
 	for(auto &&c: childs) 
 	{
@@ -137,7 +136,7 @@ std::experimental::optional<const radix_tree*> radix_tree::parameter_matches(std
             return tptr;
         }
 	}
-	return std::experimental::optional<const radix_tree*>{};
+	return radix_tree::optional_tree{};
 }
 
 std::unique_ptr<node_interface> radix_tree::get(const std::string &str, http::http_request *r) const
