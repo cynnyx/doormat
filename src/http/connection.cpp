5ad34d7ce00ba3cd98ac936eee42bdb32e6c3840
Paolo Monteverde <paolo.monteverde@cynny.com>
2017-05-09 16:14:45 +0200

    fixed connection.h/cpp indentation

--- src/http/connection.cpp
+++ src/http/connection.cpp
@@ -7,52 +7,51 @@ namespace http
 {
 http_request* connection::request_received()
 {
-    auto req_handler = std::make_shared<http::request>(this->shared_from_this());
-    auto res_handler = std::make_shared<http::response>([s = this->shared_from_this()](){
-        s->notify_response();
-    });
-    if(request_cb) (*request_cb)(req_handler, res_handler);
-    requests.emplace(std::move(req_handler));
-    responses.push(std::move(res_handler));
-    return &current_request;
+	auto req_handler = std::make_shared<http::request>(this->shared_from_this());
+	auto res_handler = std::make_shared<http::response>([s = this->shared_from_this()](){
+		s->notify_response();
+	});
+	if(request_cb) (*request_cb)(req_handler, res_handler);
+	requests.emplace(std::move(req_handler));
+	responses.push(std::move(res_handler));
+	return &current_request;
 }
 
 
 http_request* connection::request_received(std::function<void(http_response&&)> hcb, std::function<void(dstring&&)> bcb, std::function<void(dstring&&, dstring&&)> tcb, std::function<void()> ccb)
 {
-    auto req_handler = std::make_shared<http::request>(this->shared_from_this());
-    auto res_handler = std::make_shared<http::response>(hcb, bcb, tcb, ccb);
+	auto req_handler = std::make_shared<http::request>(this->shared_from_this());
+	auto res_handler = std::make_shared<http::response>(hcb, bcb, tcb, ccb);
 
-    if(request_cb) (*request_cb)(req_handler, res_handler);
-    requests.emplace(std::move(req_handler));
-    responses.push(std::move(res_handler));
+	if(request_cb) (*request_cb)(req_handler, res_handler);
+	requests.emplace(std::move(req_handler));
+	responses.push(std::move(res_handler));
 
-    return &current_request;
+	return &current_request;
 }
 
 
-std::tuple<std::function<void()>, std::function<void(dstring&&)>, std::function<void(dstring&&, dstring&&)>, std::function<void()>>
-connection::get_request_handlers()
+std::tuple<std::function<void()>, std::function<void(dstring&&)>, std::function<void(dstring&&, dstring&&)>, std::function<void()>> connection::get_request_handlers()
 {
-    auto hcb = [this](){ notify_headers();};
-    auto bcb = [this](dstring&&b){notify_body(std::move(b));};
-    auto tcb = [this](dstring&&b, dstring&&c){notify_trailers(std::move(b), std::move(c));};
-    auto ccb = [this](){notify_finished(); };
-    return std::make_tuple(hcb, bcb, tcb, ccb);
-};
+	auto hcb = [this](){ notify_headers();};
+	auto bcb = [this](dstring&&b){notify_body(std::move(b));};
+	auto tcb = [this](dstring&&b, dstring&&c){notify_trailers(std::move(b), std::move(c));};
+	auto ccb = [this](){notify_finished(); };
+	return std::make_tuple(hcb, bcb, tcb, ccb);
+}
 
 
 /** \brief method called once current_request contains the http preamble and all the headers readed from the socket.
  * */
 void connection::notify_headers()
 {
-    auto &last_req = requests.back();
-    if(auto s = last_req.lock()) //notify data to alive requests.
-    {
-        s->headers(std::move(current_request));
-    }
-    /** re-assign current_request so that it is valid again*/
-    current_request = http_request{};
+	auto &last_req = requests.back();
+	if(auto s = last_req.lock()) //notify data to alive requests.
+	{
+		s->headers(std::move(current_request));
+	}
+	/** re-assign current_request so that it is valid again*/
+	current_request = http_request{};
 }
 
 /** \brief method called every time a portion of the body has been readed.
@@ -60,77 +59,77 @@ void connection::notify_headers()
  * */
 void connection::notify_body(dstring&& body)
 {
-    auto &last_req = requests.back();
-    if(auto s = last_req.lock()) //notify data to alive requests.
-    {
-        s->body(std::move(body));
-    }
+	auto &last_req = requests.back();
+	if(auto s = last_req.lock()) //notify data to alive requests.
+	{
+		s->body(std::move(body));
+	}
 }
 
 void connection::notify_finished()
 {
-    auto &last_req = requests.back();
-    if(auto s = last_req.lock()) //notify data to alive requests.
-    {
-        s->finished();
-    }
+	auto &last_req = requests.back();
+	if(auto s = last_req.lock()) //notify data to alive requests.
+	{
+		s->finished();
+	}
 }
 
 void connection::notify_trailers(dstring &&k, dstring &&v)
 {
-    auto &last_req = requests.back();
-    if(auto s = last_req.lock()) //notify data to alive requests.
-    {
-        s->trailer(std::move(k), std::move(v));
-    }
+	auto &last_req = requests.back();
+	if(auto s = last_req.lock()) //notify data to alive requests.
+	{
+		s->trailer(std::move(k), std::move(v));
+	}
 }
 
 void connection::notify_error(bool global)
 {
-    if(global) {
-        while(!requests.empty())
-        {
-            if(auto s = requests.front().lock()) s->error();
-            requests.pop();
-        }
-    }
+	if(global) {
+		while(!requests.empty())
+		{
+			if(auto s = requests.front().lock()) s->error();
+			requests.pop();
+		}
+	}
 }
 
 bool connection::poll_response(std::shared_ptr<response> res) {
-    auto state = res->get_state();
-    while(state != http::response::state::pending) {
-        switch(state) {
-            case http::response::state::headers_received:
-                notify_response_headers(res->get_headers()); break;
-            case http::response::state::body_received:
-                notify_response_body(res->get_body()); break;
-            case http::response::state::trailer_received:
-            {
-                auto trailer = res->get_trailer();
-                notify_response_trailer(std::move(trailer.first), std::move(trailer.second));
-                break;
-            }
-            case http::response::state::ended:
-                notify_response_end();
-                return true;
-            default: assert(0);
-        }
-        state = res->get_state();
-    }
-    return false;
+	auto state = res->get_state();
+	while(state != http::response::state::pending) {
+		switch(state) {
+		case http::response::state::headers_received:
+			notify_response_headers(res->get_headers()); break;
+		case http::response::state::body_received:
+			notify_response_body(res->get_body()); break;
+		case http::response::state::trailer_received:
+		{
+			auto trailer = res->get_trailer();
+			notify_response_trailer(std::move(trailer.first), std::move(trailer.second));
+			break;
+		}
+		case http::response::state::ended:
+			notify_response_end();
+			return true;
+		default: assert(0);
+		}
+		state = res->get_state();
+	}
+	return false;
 }
 
 void connection::notify_response()
 {
-    while(!responses.empty())
-    {
-        //replace all this with a polling mechanism!
-        auto &c = responses.front();
-        if(auto s = c.lock()) {
-            if(poll_response(s)) responses.pop();
-            else break;
-        }
-    }
+	while(!responses.empty())
+	{
+		//replace all this with a polling mechanism!
+		auto &c = responses.front();
+		if(auto s = c.lock()) {
+			if(poll_response(s)) responses.pop();
+			else break;
+		}
+	}
 }
 
 }
--- src/http/connection.h
+++ src/http/connection.h
@@ -11,44 +11,44 @@ class request;
 class response;
 
 struct connection : std::enable_shared_from_this<connection> {
-    using request_callback = std::function<void(std::shared_ptr<http::request>, std::shared_ptr<http::response>)>;
-    void on_request(request_callback rcb) { request_cb.emplace(std::move(rcb)); }
-    virtual void close() = 0;
-    virtual ~connection() = default;
-    std::experimental::optional<request_callback> request_cb;
+	using request_callback = std::function<void(std::shared_ptr<http::request>, std::shared_ptr<http::response>)>;
+	void on_request(request_callback rcb) { request_cb.emplace(std::move(rcb)); }
+	virtual void close() = 0;
+	virtual ~connection() = default;
+	std::experimental::optional<request_callback> request_cb;
 protected:
-    http_request *request_received();
-    http_request *request_received(std::function<void(http_response&&)>, std::function<void(dstring&&)>, std::function<void(dstring&&, dstring&&)>, std::function<void()>);
+	http_request *request_received();
+	http_request *request_received(std::function<void(http_response&&)>, std::function<void(dstring&&)>, std::function<void(dstring&&, dstring&&)>, std::function<void()>);
 
-    std::tuple<
-            std::function<void()>,
-            std::function<void(dstring&&)>,
-            std::function<void(dstring&&, dstring&&)>,
-            std::function<void()>
-    > get_request_handlers();
+	std::tuple<
+	std::function<void()>,
+	std::function<void(dstring&&)>,
+	std::function<void(dstring&&, dstring&&)>,
+	std::function<void()>
+	> get_request_handlers();
 
-    //TODO: make pure virtual as soon as we get to fix http2 implementation
-    virtual void notify_response_headers(http_response &&res){};
-    virtual void notify_response_body(dstring&& body){};
-    virtual void notify_response_trailer(dstring&&k, dstring&&v){};
-    virtual void notify_response_end(){};
+	//TODO: make pure virtual as soon as we get to fix http2 implementation
+	virtual void notify_response_headers(http_response &&res){}
+	virtual void notify_response_body(dstring&& body){}
+	virtual void notify_response_trailer(dstring&&k, dstring&&v){}
+	virtual void notify_response_end(){}
 private:
 
 
-    /** Inner management of http events, so that we can send events to the request*/
-    void notify_headers();
-    void notify_body(dstring&& body);
-    void notify_finished();
-    void notify_trailers(dstring &&, dstring&&);
-    void notify_error(bool global = false);
+	/** Inner management of http events, so that we can send events to the request*/
+	void notify_headers();
+	void notify_body(dstring&& body);
+	void notify_finished();
+	void notify_trailers(dstring &&, dstring&&);
+	void notify_error(bool global = false);
 
 
-    bool poll_response(std::shared_ptr<response>);
-    void notify_response();
+	bool poll_response(std::shared_ptr<response>);
+	void notify_response();
 
-    http_request current_request;
-    std::queue<std::weak_ptr<http::request>> requests;
-    std::queue<std::weak_ptr<http::response>> responses;
+	http_request current_request;
+	std::queue<std::weak_ptr<http::request>> requests;
+	std::queue<std::weak_ptr<http::response>> responses;
 };
 
 }
