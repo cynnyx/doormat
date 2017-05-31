3a38f4ae09be69d528ddeaf168ea46e82ad9c760
Tazio Ceri <tazio.ceri@cynny.com>
2017-05-31 11:11:54 +0200

    WIP HTTP2 test

--- test/http/server/http2_test.cpp
+++ test/http/server/http2_test.cpp
@@ -1,3 +1,4 @@
+#include <nghttp2/nghttp2.h>
 #include <gtest/gtest.h>
 #include "../../../src/http/server/request.h"
 #include "../../../src/http/server/response.h"
@@ -5,8 +6,294 @@
 #include "../src/http2/session.h"
 #include "../src/http2/stream.h"
 
+// #include "../../../deps/nghttp2/build/include/nghttp2/nghttp2.h"
+
 #include <memory>
 #include <utility>
+
+// Sorry man!
+#define MAKE_NV(NAME, VALUE)                                                   \
+  {                                                                            \
+    (uint8_t *) NAME, (uint8_t *)VALUE, sizeof(NAME) - 1, sizeof(VALUE) - 1,   \
+        NGHTTP2_NV_FLAG_NONE                                                   \
+  }
+
+#define MAKE_NV_CS(NAME, VALUE)                                                \
+  {                                                                            \
+    (uint8_t *) NAME, (uint8_t *)VALUE, sizeof(NAME) - 1, strlen(VALUE),       \
+        NGHTTP2_NV_FLAG_NONE                                                   \
+  }
+
+static void diec(const char *func, int error_code) 
+{
+	fprintf(stderr, "FATAL: %s: error_code=%d, msg=%s\n", func, error_code,
+		nghttp2_strerror(error_code));
+	//FAIL();
+}
+
+using server_connection_t = http2::session;
+
+class http2_test : public ::testing::Test
+{
+	static http2_test* tx;
+public:
+	http2_test() { tx = this; }
+	void SetUp()
+	{
+		_write_cb = [](dstring) {};
+		mock_connector = std::make_shared<MockConnector>(io, _write_cb);
+		_handler = std::make_shared<server_connection_t>();
+		mock_connector->handler(_handler);
+		response_raw.clear(); 
+		request_raw.clear();
+		go_away = false;
+	}
+
+	boost::asio::io_service io;
+	std::shared_ptr<MockConnector> mock_connector;
+	std::shared_ptr<server_connection_t> _handler;
+	MockConnector::wcb _write_cb;
+ 	std::string response_raw;
+ 	std::string request_raw;
+	bool go_away{false};
+	~http2_test() { tx = nullptr; }
+	
+	static ssize_t read( char* buf, std::size_t len )
+	{
+		ssize_t r = tx->response_raw.size();
+		r =  len < r ? len : r;
+		memcpy(buf, tx->response_raw.data(), r );
+		if ( r > 0 ) // TODO SEGMENTTION FAULT HERE, WHY?
+			tx->response_raw = tx->response_raw.erase(0, r);
+		return r;
+	}
+	
+	static ssize_t write( const char* buf, std::size_t len )
+	{
+		std::string local_buf{buf, len};
+		tx->request_raw += local_buf; // Append truncates nulls!
+		return len;
+	}
+};
+
+http2_test* http2_test::tx;
+
+struct Connection 
+{
+	nghttp2_session *session;
+};
+
+struct Request 
+{
+	const char *host;
+	/* In this program, path contains query component as well. */
+	const char *path;
+	/* This is the concatenation of host and port with ":" in
+		between. */
+	const char *hostport;
+	/* Stream ID for this request. */
+	int32_t stream_id;
+	uint16_t port;
+};
+
+int test_read(char* buf, std::size_t len )
+{
+	fprintf(stderr, "Reading");
+	return http2_test::read( buf, len );
+}
+
+int test_write( const char* buf, std::size_t len )
+{
+	fprintf(stderr, "Writing");
+	return http2_test::write( buf, len );
+}
+
+static ssize_t send_callback( nghttp2_session *session, 
+	const uint8_t *data, size_t length, int flags, void * user_data )
+{
+	int rv = test_write( (const char*)data, length );
+	return rv;
+}
+
+static ssize_t recv_callback(nghttp2_session *session , uint8_t *buf,
+	size_t length, int flags, void *user_data) 
+{
+	int rv = test_read( (char*)buf, length );
+	return rv;
+}
+
+static int on_stream_close_callback(nghttp2_session *session, int32_t stream_id,
+	uint32_t error_code, void *user_data) 
+{
+	struct Request *req;
+	req = static_cast<Request*> ( nghttp2_session_get_stream_user_data(session, stream_id) );
+	if (req) 
+	{
+		int rv = nghttp2_session_terminate_session(session, NGHTTP2_NO_ERROR);
+
+		if (rv != 0)
+			diec("nghttp2_session_terminate_session", rv);
+	}
+	return 0;
+}
+
+static int on_frame_send_callback(nghttp2_session *session,
+	const nghttp2_frame *frame, void *user_data) 
+{
+	size_t i;
+	switch (frame->hd.type) 
+	{
+	case NGHTTP2_HEADERS:
+		if (nghttp2_session_get_stream_user_data(session, frame->hd.stream_id)) 
+		{
+			const nghttp2_nv *nva = frame->headers.nva;
+			printf("[INFO] C ----------------------------> S (HEADERS)\n");
+			for (i = 0; i < frame->headers.nvlen; ++i) 
+			{
+				fwrite(nva[i].name, nva[i].namelen, 1, stdout);
+				printf(": ");
+				fwrite(nva[i].value, nva[i].valuelen, 1, stdout);
+				printf("\n");
+			}
+		}
+		break;
+	case NGHTTP2_RST_STREAM:
+		printf("[INFO] C ----------------------------> S (RST_STREAM)\n");
+		break;
+	case NGHTTP2_GOAWAY:
+		printf("[INFO] C ----------------------------> S (GOAWAY)\n");
+		break;
+	}
+	return 0;
+}
+
+
+static int on_frame_recv_callback(nghttp2_session *session,
+	const nghttp2_frame *frame, void *user_data ) 
+{
+	size_t i;
+	switch (frame->hd.type) 
+	{
+	case NGHTTP2_HEADERS:
+	if (frame->headers.cat == NGHTTP2_HCAT_RESPONSE) 
+	{
+			const nghttp2_nv *nva = frame->headers.nva;
+			Request *req;
+			req = static_cast<Request*>( nghttp2_session_get_stream_user_data(session, frame->hd.stream_id) );
+			if (req) 
+			{
+				printf("[INFO] C <---------------------------- S (HEADERS)\n");
+				for (i = 0; i < frame->headers.nvlen; ++i) 
+				{
+					fwrite(nva[i].name, nva[i].namelen, 1, stdout);
+					printf(": ");
+					fwrite(nva[i].value, nva[i].valuelen, 1, stdout);
+					printf("\n");
+				}
+			}
+		}
+		break;
+		case NGHTTP2_RST_STREAM:
+		printf("[INFO] C <---------------------------- S (RST_STREAM)\n");
+		break;
+		case NGHTTP2_GOAWAY:
+		printf("[INFO] C <---------------------------- S (GOAWAY)\n");
+		break;
+	}
+	return 0;
+}
+
+/*
+ * The implementation of nghttp2_on_data_chunk_recv_callback type. We
+ * use this function to print the received response body.
+ */
+static int on_data_chunk_recv_callback(nghttp2_session *session,
+	uint8_t flags, int32_t stream_id, const uint8_t *data, size_t len,
+	void *user_data) 
+{
+	Request *req;
+	req = static_cast<Request *>( nghttp2_session_get_stream_user_data(session, stream_id) );
+	if (req)
+	{
+		printf("[INFO] C <---------------------------- S (DATA chunk)\n"
+			"%lu bytes\n",
+			(unsigned long int)len);
+		fwrite(data, 1, len, stdout);
+		printf("\n");
+	}
+	return 0;
+}
+
+static void setup_nghttp2_callbacks(nghttp2_session_callbacks *callbacks) 
+{
+	nghttp2_session_callbacks_set_send_callback(callbacks, send_callback);
+	nghttp2_session_callbacks_set_recv_callback(callbacks, recv_callback);
+	nghttp2_session_callbacks_set_on_frame_send_callback(callbacks,
+		on_frame_send_callback);
+	nghttp2_session_callbacks_set_on_frame_recv_callback(callbacks,
+		on_frame_recv_callback);
+	nghttp2_session_callbacks_set_on_stream_close_callback(
+		callbacks, on_stream_close_callback);
+	nghttp2_session_callbacks_set_on_data_chunk_recv_callback(
+		callbacks, on_data_chunk_recv_callback);
+}
+
+static void exec_io(struct Connection *connection) 
+{
+	int rv = nghttp2_session_recv(connection->session);
+	if (rv != 0)
+		diec("nghttp2_session_recv", rv);
+
+	rv = nghttp2_session_send(connection->session);
+	if (rv != 0)
+		diec("nghttp2_session_send", rv);
+}
+
+static std::unique_ptr<Connection> start_client()
+{
+	nghttp2_session_callbacks *callbacks;
+	
+	std::unique_ptr<Connection> c{ new Connection };
+	Connection* connection = c.get();
+	ssize_t rv = nghttp2_session_callbacks_new(&callbacks);
+	if ( rv > 0 ) return std::unique_ptr<Connection>{};
+	setup_nghttp2_callbacks(callbacks);
+	
+	rv = nghttp2_session_client_new(&(connection->session), callbacks, &connection);
+	if ( rv > 0 ) return std::unique_ptr<Connection>{};
+	nghttp2_session_callbacks_del(callbacks);
+	return c;
+}
+
+/*
+ * Submits the request |req| to the connection |connection|.  This
+ * function does not send packets; just append the request to the
+ * internal queue in |connection->session|.
+ */
+static void submit_request(struct Connection *connection, struct Request *req) 
+{
+	int32_t stream_id;
+	/* Make sure that the last item is NULL */
+	const nghttp2_nv nva[] = {
+		MAKE_NV(":method", "GET"), MAKE_NV_CS(":path", req->path),
+		MAKE_NV(":scheme", "https"), MAKE_NV_CS(":authority", req->hostport),
+		MAKE_NV("accept", "*/*"),
+		MAKE_NV("user-agent", "nghttp2/" NGHTTP2_VERSION)};
+
+	stream_id = nghttp2_submit_request(connection->session, NULL, nva,
+		sizeof(nva) / sizeof(nva[0]), NULL, req);
+
+	if (stream_id < 0)
+		diec("nghttp2_submit_request", stream_id);
+
+	req->stream_id = stream_id;
+	printf("[INFO] Stream ID = %d\n", stream_id);
+}
+
+static void submit_settings(struct Connection* connection)
+{
+	nghttp2_submit_settings(connection->session, NGHTTP2_FLAG_NONE, NULL, 0);
+}
 
 // TODO this is a duplicate - bring it out
 static std::unique_ptr<char[]> make_data_ptr(const std::string& s)
@@ -16,26 +303,60 @@ static std::unique_ptr<char[]> make_data_ptr(const std::string& s)
 	return std::move(ptr);
 }
 
-using server_connection_t = http2::session;
-
-class http2_test : public ::testing::Test
+TEST_F(http2_test, connection)
 {
-public:
-	void SetUp()
+	// pass this as answer to http2 stuff
+	_write_cb = [this](dstring d)
 	{
-		_write_cb = [](dstring) {};
-		mock_connector = std::make_shared<MockConnector>(io, _write_cb);
-		_handler = std::make_shared<server_connection_t>();
-		mock_connector->handler(_handler);
-		response = "";
-	}
+		std::string chunk = d;
+		response_raw += chunk;
+	};
+	
+	_handler->on_request([&](auto conn, auto req, auto res) 
+	{
+		req->on_finished([res](auto req) 
+		{
+			http::http_response r;
+			r.protocol(http::proto_version::HTTP20);
+			std::string body{"Ave client, dummy node says hello"};
+			r.status(200);
+			r.header("content-type", "text/plain");
+			r.header("date", "Tue, 17 May 2016 14:53:09 GMT");
+			r.content_len(body.size());
+			res->headers(std::move(r));
+			res->body(make_data_ptr(body), body.size());
+			res->end();
+		});
+	});
+	
+	bool terminated{false};
+	mock_connector->io_service().post([this, &terminated]() 
+	{
+		Request req;
+		Request* greq = &req;
+		greq->host = "www.cristo.it";
+		greq->port = 80;
+		greq->path = "/";
+		greq->stream_id = -1;
+		
+		std::unique_ptr<Connection>  c = start_client();
+		Connection* cnx =  c.get();
+		submit_settings(cnx);
+		
+		submit_request(cnx, greq);
 
-	boost::asio::io_service io;
-	std::shared_ptr<MockConnector> mock_connector;
-	std::shared_ptr<server_connection_t> _handler;
-	MockConnector::wcb _write_cb;
-	std::string response;
-};
+		while (nghttp2_session_want_read(cnx->session) ||
+			nghttp2_session_want_write(cnx->session)) 
+		{
+			exec_io( cnx );
+			mock_connector->read( request_raw );
+			request_raw = "";
+		}
+		nghttp2_session_del( cnx->session );
+	});
+	mock_connector->io_service().run();
+	ASSERT_TRUE( terminated );
+}
 
 TEST_F(http2_test, randomdata)
 {
@@ -100,7 +421,7 @@ TEST_F(http2_test, getting_200_at_once)
 		{
 			bool eq{true};
 			for ( std::size_t i = 0; i < len_resp && eq; ++i )
-				eq = actual_res.cdata()[i] == raw_response[i];
+				eq |= actual_res.cdata()[i] == raw_response[i];
 			done = true;
 		}
 	};
@@ -128,5 +449,6 @@ TEST_F(http2_test, getting_200_at_once)
 	ASSERT_TRUE(done);	
 	// TODO GO AWAY IS MISSING 
 // 	ASSERT_TRUE(_handler->should_stop());
-
 }
+
+
