0e95673b74fc6a958c73d7b08ed5e7c695a02aa1
Paolo Monteverde <paolo.monteverde@cynny.com>
2017-05-31 14:20:17 +0200

    removed deleted headers

--- include/doormat/doormat.hpp
+++ include/doormat/doormat.hpp
@@ -1,14 +1,8 @@
 #ifndef DOORMAT_DOORMAT_HPP_
 #define DOORMAT_DOORMAT_HPP_
 
-#include "chain.hpp"
-#include "configuration.hpp"
 #include "http.hpp"
-#include "io_service_pool.hpp"
-#include "node_interface.hpp"
 #include "server.hpp"
-#include "service.hpp"
-#include "stats.hpp"
 #include "client.hpp"
 #include "utils.hpp"
 #include "errors.hpp"
