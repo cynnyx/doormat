456033e534e370abc52bba265b96be0bcae3bef5
Tazio Ceri <tazio.ceri@cynny.com>
2017-06-20 16:49:23 +0200

    Tabs and not spaces, WTF!

--- test/mocks/mock_connector/mock_connector.h
+++ test/mocks/mock_connector/mock_connector.h
@@ -8,21 +8,23 @@ struct MockConnector : server::connector_interface, std::enable_shared_from_this
 {
 	using wcb = std::function<void(std::string)>;
 
-    MockConnector(boost::asio::io_service &io, wcb& cb);
+	MockConnector(boost::asio::io_service &io, wcb& cb);
 	~MockConnector();
 
-    void do_write() override;
-    void do_read() override;
-    boost::asio::ip::address origin() const override;
-    bool is_ssl() const noexcept override;
-    void close() override;
-    boost::asio::io_service &io_service() override;
+	void do_write() override;
+	void do_read() override;
+	boost::asio::ip::address origin() const override;
+	bool is_ssl() const noexcept override;
+	void close() override;
+	boost::asio::io_service &io_service() override;
 	void set_timeout(std::chrono::milliseconds) override;
 	void handler(std::shared_ptr<server::http_handler>) override;
 	void start(bool) override;
 	void read(std::string request);
 
-    boost::asio::io_service &io;
-    wcb& write_cb;
-    std::shared_ptr<server::http_handler> _handler;
+	boost::asio::io_service &io;
+	wcb& write_cb;
+	std::shared_ptr<server::http_handler> _handler;
+	
+	bool die{false};
 };
